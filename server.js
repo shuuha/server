bb91dc7fef06633e5111c69259997b21c33dc281
shuha <s.shuuha@gmail.com>
2017-09-28 15:50:50 +0500

    init

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-app.listen(8080, () => {
-    console.log('listening on port 8080');
+app.listen(3000, () => {
+    console.log('listening on port 3000');
 })
