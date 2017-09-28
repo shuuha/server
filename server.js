3d6735508edc2b29e9e0862b4166f4dfa3b01a79
shuha <s.shuuha@gmail.com>
2017-09-28 15:33:40 +0500

    int

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log(req.url);
     res.send('Hello')
 })
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
+    console.log('listening on port 8080');
 })
