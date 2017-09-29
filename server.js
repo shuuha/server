21d256598a6a7ec96fdeca04fda1620277cd148a
shuha <s.shuuha@gmail.com>
2017-09-29 19:17:39 +0500

    init

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express');
 // const routes = require('./config/routes');
 
-var MongoClient = require('mongodb').mongoClient;
+var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/data';
 
 
 const app = express();
 
 app.get('/', (req,res) => {
-    MongoClient(url, (err, db) => {
-        if(err) console.log('error', err);
-        else {            
-            console.log('connection established');
-            res.end('mongo db');
-        }
-    })
+    // MongoClient(url, (err, db) => {
+    //     if(err) console.log('error', err);
+    //     else {            
+    //         console.log('connection established');
+    //         res.end('mongo db');
+    //     }
+    // })
+    res.end('hello');
 })
 
 app.post('/', (req, res) => {
