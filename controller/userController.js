80aeeba73408b199275b8e1a0337734865d9f3ce
shuha <s.shuuha@gmail.com>
2017-10-01 03:49:07 +0500

    i

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,15 @@
 const db = require('../models/db');
 
 class UserController{
-    get(req, res){        
-        db.getAll()
+    get(req, res){
+        db.connect(db.getAll)
             .then(result => {
                 if(result.length)
                     res.end(JSON.stringify(result));
 
                 else 
                     res.end('nothing found');
-            })       
+            })
     }
 
     post(){
--- models/db.js
+++ models/db.js
@@ -6,8 +6,14 @@ class MongoDbRepository{
         this.URL = 'mongodb://localhost:27017/data';        
     }    
 
-    connect(){
-        return MongoClient.connect(this.URL);
+    connect(func){
+        return MongoClient.connect(this.URL)
+                .then(db => this.db = db)
+                .then(func)
+                .then(result => { 
+                        this.db.close(); 
+                        return result})
+                .catch(this.err);
     }
 
     update(){
@@ -18,31 +24,26 @@ class MongoDbRepository{
         this.db.close();        
     }
 
-    result(result){
-        console.log(result);
-        return result;
+    err(err){
+        console.log(err)
     }
 
     getAll(){
-        return this.connect()            
-            .then(db => {
-                    const result = db.collection('data').find({}).toArray();
-                    db.close();
-                    return result
-                })            
-            .then(this.result)
-            .catch(err => console.log(err));
-
-        // return this.connect()
-        //         .then(arg => this.db.collection('data').find({}).toArray((err, r)=> {
-        //             if(err)
-        //                 console.log('error in getting all the data');
-        //                 return r
-                    
-        //             this.db.close();                
-        //         }))
-                // .catch(err => console.log(err));
+        return this.db.collection('data').find({}).toArray();
     }
+
+    // getAll(){
+    //     return this.connect()
+    //         .then(db => {
+    //                 const result = db.collection('data').find({}).toArray();
+    //                 db.close();
+    //                 return result
+    //             })
+    //         .then(res => res)
+    //         .catch(err => console.log(err));
+    // }
+
+
 
     insert(){
         this.connect();
