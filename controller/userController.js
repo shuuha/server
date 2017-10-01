130754631628c81edbf2e84344e8406eb90722c0
shuha <s.shuuha@gmail.com>
2017-10-01 14:49:45 +0500

    i

--- controller/userController.js
+++ controller/userController.js
@@ -23,7 +23,7 @@ class UserController{
 
     delete(req, res){        
         db.connect( ()=> db.remove(req.params))
-            .then( rez => res.end('deleted a document with params: ', console.log(rez.result)));
+            .then( ({result}) => res.end('deleted a document with params: ', console.log(result)));
     }
 }
 
