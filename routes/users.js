093710c1d457c389c1dd9d89862521b3b8aac2be
shuha <s.shuuha@gmail.com>
2017-10-03 02:31:21 +0500

    i

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const userController = require('../controller/userController');
 
-router.get('/:id?', userController.get);
+router.get('/:id', userController.get);
 router.post('/', userController.post);
 router.delete('/:id/:page', userController.delete);
 
