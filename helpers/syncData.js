e8a1b16c3956f50f63fa4c43f03b9db58cbb0cc4
shuha <s.shuuha@gmail.com>
2017-10-02 19:40:41 +0500

    i

--- helpers/syncData.js
+++ helpers/syncData.js
@@ -1,18 +1,6 @@
 let initialData = require('../data/initialData');    // raw data (client skeleton )
 
 function syncData(dbData){
-    // const dataNew = {};
-
-    // inititalData.Object.keys().forEach( q, page => {
-    //     if(q.page == dbData.page){
-    //         q.inputs.Object.keys().forEach( i, input => {
-                // initialData.Object.keys()[page].inputs.Object.keys()[input].value =  dbData[input].value;
-
-    //         })
-    //     }
-    // })
-    console.log('syncData in action');
-    
     if(dbData.length){
     dbData.forEach((q, pageIndex) => { 
             q.inputs.forEach( (i, inputIndex) => {
