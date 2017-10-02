c0a2575030c37f126875957de26f46e16d8666be
shuha <s.shuuha@gmail.com>
2017-10-02 20:51:32 +0500

    i

--- helpers/syncData.js
+++ helpers/syncData.js
@@ -5,7 +5,7 @@ function syncData(dbData){
     if(dbData.length){
     dbData.forEach((q, pageIndex) => { 
             q.inputs.forEach( (i, inputIndex) => {
-            console.log(initialData[0].inputs[0].value);
+            console.log(initialData["0"].inputs["0"].value);
         // Object.assign(initialData[pageIndex].inputs[inputIndex].value, i.value)
             })
         })
