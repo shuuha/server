90483e6877e3c2fe91155b9ae41c485d86e3cc43
shuha <s.shuuha@gmail.com>
2017-10-02 20:05:39 +0500

    i

--- helpers/syncData.js
+++ helpers/syncData.js
@@ -1,8 +1,9 @@
 let initialData = require('../data/initialData');    // raw data (client skeleton )
 
 function syncData(dbData){
-    if(dbData.length){
         console.log('syncdata in action', dbData);
+
+    if(dbData.length){
     dbData.forEach((q, pageIndex) => { 
             q.inputs.forEach( (i, inputIndex) => {
         Object.assign(initialData[pageIndex].inputs[inputIndex].value, q.page.inputs[inputIndex].value)
